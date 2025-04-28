9ceec29c4a7b14ffac7e40e95c3b7ab0a26f34ac
Anuj C More <moreanuj1307@gmail.com>
2025-04-27 19:02:51 -0700

    Update: Transaction history design changes

--- src/screens/ServiceProvider/TransactionHistory.tsx
+++ src/screens/ServiceProvider/TransactionHistory.tsx
@@ -13,41 +13,7 @@ import {useSelector} from 'react-redux';
 export const TransactionHistory = () => {
   const user = useSelector((state: RootState) => state.auth.user);
 
-  const [transactionHistory, setTransactionHistory] = useState([
-    {
-      userId: '1df1aaba-153a-4e70-9e01-0e831ae3fd26',
-      serviceId: '4bbc181c-1d0f-40a4-a50a-a56ec2521e59',
-      title: 'Dog Service',
-      description: 'We provide dog service',
-      totalAmt: '180.00',
-      transactionType: 'cash',
-      status: 'confirmed',
-      transactionId: '121223434787823',
-      transactionDateTime: '2025-02-10 00:28:36.635',
-    },
-    {
-      userId: '1df1aaba-153a-4e70-9e01-0e831ae3fd26',
-      serviceId: 'f8539bd6-11c7-4ee4-a6a7-9eb7342cc50b',
-      title: 'Flower Service',
-      description: 'We provide flower service',
-      totalAmt: '180.00',
-      transactionType: 'card',
-      status: 'pending',
-      transactionId: '121223434787824',
-      transactionDateTime: '2025-02-10 00:28:36.635',
-    },
-    {
-      userId: '1df1aaba-153a-4e70-9e01-0e831ae3fd26',
-      serviceId: 'a1239bd6-22c7-4ee4-b6a7-9eb7342cc50c',
-      title: 'Flower Service',
-      description: 'We provide flower service',
-      totalAmt: '180.00',
-      transactionType: 'paypal',
-      status: 'cancelled',
-      transactionId: '121223434787825',
-      transactionDateTime: '2025-02-10 00:28:36.635',
-    },
-  ]);
+  const [transactionHistory, setTransactionHistory] = useState([]);
 
   const navigation = useNavigation();
 
@@ -57,7 +23,7 @@ export const TransactionHistory = () => {
   };
 
   const apiCall = async () => {
-    const response = await getTransactions(user.id);
+    const response = await getTransactions(user.id);   
     if (response.data) {
       setTransactionHistory(response.data);
     }
@@ -70,8 +36,8 @@ export const TransactionHistory = () => {
     <View style={globalStyle.globalContainer}>
       {transactionHistory.length === 0 ? (
         <FallBack
-          heading={WORD_DIR.emptyCartHeading}
-          subHeading={WORD_DIR.emptyCartSubHeading}
+          heading={WORD_DIR.noTransaction}
+          subHeading={WORD_DIR.noTransactionSubHeading}
           imageSrc={noRecordFound}
         />
       ) : (
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -19,3 +19,39 @@ export const USER_DETAILS = {
 };
 
 export const MAX_SCHEDULE_DISPLAY = 5;
+
+export const  DUMMY_DATA = [
+  {
+    userId: '1df1aaba-153a-4e70-9e01-0e831ae3fd26',
+    serviceId: '4bbc181c-1d0f-40a4-a50a-a56ec2521e59',
+    title: 'Dog Service',
+    description: 'We provide dog service',
+    totalAmt: '180.00',
+    transactionType: 'cash',
+    status: 'confirmed',
+    transactionId: '121223434787823',
+    transactionDateTime: '2025-02-10 00:28:36.635',
+  },
+  {
+    userId: '1df1aaba-153a-4e70-9e01-0e831ae3fd26',
+    serviceId: 'f8539bd6-11c7-4ee4-a6a7-9eb7342cc50b',
+    title: 'Flower Service',
+    description: 'We provide flower service',
+    totalAmt: '180.00',
+    transactionType: 'card',
+    status: 'pending',
+    transactionId: '121223434787824',
+    transactionDateTime: '2025-02-10 00:28:36.635',
+  },
+  {
+    userId: '1df1aaba-153a-4e70-9e01-0e831ae3fd26',
+    serviceId: 'a1239bd6-22c7-4ee4-b6a7-9eb7342cc50c',
+    title: 'Flower Service',
+    description: 'We provide flower service',
+    totalAmt: '180.00',
+    transactionType: 'paypal',
+    status: 'cancelled',
+    transactionId: '121223434787825',
+    transactionDateTime: '2025-02-10 00:28:36.635',
+  },
+];
